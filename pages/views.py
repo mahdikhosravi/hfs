a926d6b5aa37661103456792c423618cd0f42d1f
Mahdi Khosravi <mahdi0khosravi@gmail.com>
2013-12-27 10:18:07 +0330

    managementPage added

--- pages/views.py
+++ pages/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 class Category:
     name = None
-    id=None
+    id = None
     childs = []
     parent = None
 
     def __repr__(self):
         return self.name
+
 
 cat1 = {'name': 'فیلم و سریال', 'id': 1}
 cat2 = {'name': 'کتاب', 'id': 2}
@@ -19,11 +20,17 @@ cat6 = {'name': 'رمان', 'id': 6, 'parent': 2}
 cat7 = {'name': 'تاریخی', 'id': 7, 'parent': 2}
 cats = [cat1, cat2, cat3, cat4, cat5, cat6, cat7]
 
+
 def viewMainPage(request):
     return render(request, 'mainPage.html', {'categories': cats, 'title': 'MainPage'})
+
 
 def viewProductPage(request, cat):
     return render(request, 'productPage.html', {'categories': cats, 'title': 'ProductPage'})
 
+
 def viewSearchPage(request):
-    return render(request, 'productPage.html', {'categories': cats, 'title': 'Search'})
+    return render(request, 'productPage.html', {'categories': cats, 'title': 'SearchResultPage'})
+
+def viewManagementPage(request):
+    return render(request, 'managementPage.html', {'categories': cats, 'title': 'ManagementPage'})
